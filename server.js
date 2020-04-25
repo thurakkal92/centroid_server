3a6bcd3494a596fa9f0ce254f14205dcf1e6479d
Nabeel <nthurakkal92@gmail.com>
2020-04-26 01:28:53 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+var nodemailer = require('nodemailer');
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.post('/sendemail', sendEmail);
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  auth: {
+    user: 'mail.centroidpolymer@gmail.com',
+    pass: 'Centroid@123'
+  }
+});
+
+function sendEmail(req, res) {
+
+  var mailOptions = {
+    from: 'mail.centroidpolymer@gmail.com',
+    to: [ 'nabeel.thurakkal92@gmail.com', 'info@centroidpolymer.com', 'nikhilmathew.mec@gmail.com' ],
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.json({ status: 400, message: error }).status(400)
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({ status: 200, message: 'Email sent' }).status(200)
+    }
+    
+  });
+}
+app.get('/', function (req, res) { res.send('hello world') })
+
+app.listen(8080)
+
+
+
+
+
